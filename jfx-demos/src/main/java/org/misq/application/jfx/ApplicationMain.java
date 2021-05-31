e25b51dda873c04f3ec4ece87dd426a39f734a3b
ghubstan <36207203+ghubstan@users.noreply.github.com>
2021-05-31 15:49:18 -0300

    Add jfx-16 demo, then figure out how to make packager work with jdk 16

---
+++ jfx-demos/src/main/java/org/misq/application/jfx/ApplicationMain.java
@@ -0,0 +1,32 @@
+package org.misq.application.jfx;
+
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+
+public class ApplicationMain extends Application {
+
+    // JVM Opts:
+    // --module-path <path-to>/javafx-sdk-16/lib --add-modules=javafx.controls,javafx.fxml,javafx.graphics
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(ApplicationMain.class.getResource("/SearchController.fxml"));
+        AnchorPane page = loader.load();
+        Scene scene = new Scene(page);
+
+        scene.getStylesheets().add("/search.css");
+
+        primaryStage.setTitle("Title goes here");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
