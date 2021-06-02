898046dd34aed27d3d0b405f93dc3de37e8e37f3
chimp1984 <chimp1984@gmx.com>
2021-06-02 14:02:06 -0500

    Remove api module Strip down grpc module
    I think we do not need a dedicated api module but each domain comes with it's high level api (similar like the CoreApi in Bisq uses the other domain API classes)

--- grpc/src/main/java/org/misq/grpc/ErrorMessageHandler.java
+++
@@ -1,6 +0,0 @@
-package org.misq.grpc;
-
-// TODO Move to lower level module for interfaces used by all
-public interface ErrorMessageHandler {
-    void handleErrorMessage(String errorMessage);
-}
--- grpc/src/main/java/org/misq/grpc/GrpcServer.java
+++ grpc/src/main/java/org/misq/grpc/GrpcServer.java
@@ -4,10 +4,8 @@ package org.misq.grpc;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
 import io.grpc.protobuf.services.ProtoReflectionService;
-import org.misq.api.CoreApi;
 import org.misq.common.di.annotation.Singleton;
 import org.misq.common.threadmodel.UserThread;
-import org.misq.wallet.installer.WalletInstallerApi;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -20,41 +18,11 @@ import static java.util.concurrent.TimeUnit.SECONDS;
 public class GrpcServer {
     private static final Logger log = LoggerFactory.getLogger(GrpcServer.class);
 
-    // Use grpcurl to test endpoints.
-    // https://github.com/grpc/grpc-java/blob/master/documentation/server-reflection-tutorial.md
-    // https://offensivedefence.co.uk/posts/grpc-attack-surface
-    // Examples:
-    // $ grpcurl --plaintext   localhost:9999 list
-    //      grpc.reflection.v1alpha.ServerReflection
-    //      io.misq.protobuffer.GetVersion
-    //      io.misq.protobuffer.Help
-    //      io.misq.protobuffer.Wallets
-    //
-    // $ grpcurl --plaintext   localhost:9999 grpc.proto.Wallets/GetBalance
-    // $ grpcurl --plaintext   localhost:9999 grpc.proto.Help/GetMethodHelp
-    // $ grpcurl --plaintext   localhost:9999 grpc.proto.P2P/GetPeers
-    // $ grpcurl --plaintext   localhost:9999 grpc.proto.GetVersion/GetVersion
-    // $ grpcurl --plaintext   localhost:9999 grpc.proto.WalletInstaller/InstallWallet
-    //
-    // Describe services
-    // $ grpcurl --plaintext   localhost:9999 describe grpc.proto.P2P
-    // $ grpcurl --plaintext   localhost:9999 describe grpc.proto.Wallets
-    // $ grpcurl --plaintext   localhost:9999 describe grpc.proto.WalletInstaller
-    //
-    // Enabling gRPC reflection in Java:
-    // https://github.com/grpc/grpc-java/blob/master/documentation/server-reflection-tutorial.md#enable-server-reflection
-
     private final Server server;
 
-    public GrpcServer(CoreApi coreApi, WalletInstallerApi walletInstallerApi) {
+    public GrpcServer() {
         this.server = ServerBuilder.forPort(9999)
                 .executor(UserThread.getExecutor())
-                .addService(new GrpcHelpService(coreApi))
-                .addService(new GrpcP2PService(coreApi))
-                .addService(new GrpcVersionService(coreApi))
-                .addService(new GrpcWalletsService(coreApi))
-                .addService(new GrpcWalletInstallerService(walletInstallerApi))
-                // .intercept(new PasswordAuthInterceptor())
                 .addService(ProtoReflectionService.newInstance())
                 .build();
     }
