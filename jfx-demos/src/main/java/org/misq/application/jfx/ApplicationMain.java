b5fb069c457bf94e7ace3024bb378ccaed4952fc
ghubstan <36207203+ghubstan@users.noreply.github.com>
2021-06-02 16:57:52 -0300

    Experiment with apply from: centralized-deps gradle files

--- jfx-demos/src/main/java/org/misq/application/jfx/ApplicationMain.java
+++ jfx-demos/src/main/java/org/misq/application/jfx/ApplicationMain.java
@@ -6,9 +6,12 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 public class ApplicationMain extends Application {
+    private static final Logger log = LoggerFactory.getLogger(ApplicationMain.class);
 
     // Add JVM Opts to IDE Launcher:
     //  --module-path <path-to>/javafx-sdk-16/lib --add-modules=javafx.controls,javafx.fxml,javafx.graphics
@@ -20,11 +23,13 @@ public class ApplicationMain extends Application {
 
 
     public static void main(String[] args) {
+        log.info("launch");
         launch(args);
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        log.info("start");
         FXMLLoader loader = new FXMLLoader(ApplicationMain.class.getResource("/SearchController.fxml"));
         AnchorPane page = loader.load();
         Scene scene = new Scene(page);
